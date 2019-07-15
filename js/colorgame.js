eaac719fce924c5f6f7dba7d5c1d19e3bc0ee08e
Esteban Romo <esteban.romo.98@gmail.com>
2019-07-15 16:51:48 -0500

    Updated all
    Dynamic title color, handle rgb as separate values, shadows in title.

--- js/colorgame.js
+++ js/colorgame.js
@@ -1,5 +1,6 @@
 let squares = document.querySelectorAll(".square");
 let colorDisplay = document.querySelector("#colorDisplay");
+let titleDisplay = document.querySelector("#titleDisplay");
 let messageDisplay = document.querySelector("#messageDisplay");
 let bgContainer = document.querySelector("#bgContainer");
 let resetButton = document.querySelector("#reset");
@@ -10,7 +11,7 @@ let cant = squares.length;
 let colors = generateRandomColors(cant);
 let pickedColor = pickColor();
 let blocked = false;
-colorDisplay.textContent = pickedColor;
+colorDisplay.textContent = rgbToText(pickedColor);
 
 resetButton.addEventListener("click", function () {
     newGame(cant);
@@ -39,11 +40,12 @@ for (let i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function () {
         if (!blocked) {
             let squareColor = this.style.backgroundColor;
-            if (squareColor === pickedColor) {
+            if (squareColor === rgbToText(pickedColor)) {
                 messageDisplay.textContent = "Correct!";
                 display(cant);
-                bgContainer.style.backgroundColor = pickedColor;
-                changeColors(squareColor);
+                bgContainer.style.backgroundColor = rgbToText(pickedColor);
+                changeColors(pickedColor);
+                titleDisplay.style.color = getBack(pickedColor);
                 blocked = true;
             } else {
                 this.classList.add("hidden");
@@ -72,26 +74,37 @@ function generateRandomColors(num) {
     let arr = [];
     //repeat num times
     for (let i = 0; i < num; i++) {
-        arr.push(randomColor());
+        arr.push(function () {
+            //pick each color from 0 - 255
+            let r = Math.floor(Math.random() * 255) + 1;
+            let g = Math.floor(Math.random() * 255) + 1;
+            let b = Math.floor(Math.random() * 255) + 1;
+            //return "rgb(" + r + ", " + g + ", " + b + ")";
+            return [r, g, b];
+        }());
     }
     //return array
     return arr;
 }
-function randomColor() {
-    //pick each color from 0 - 255
-    let r = Math.floor(Math.random() * 256);
-    let g = Math.floor(Math.random() * 256);
-    let b = Math.floor(Math.random() * 256);
-    return "rgb(" + r + ", " + g + ", " + b + ")";
+
+function rgbToText(rgb) {
+    return "rgb(" + rgb[0] + ", " + rgb[1] + ", " + rgb[2] + ")";
 }
 
+function getBack(color) {
+    if ((color[0] * 0.299 + color[1] * 0.587 + color[2] * 0.114) > 186) {
+        return "#232323";
+    } else {
+        return "#f3f3f3";
+    }
+}
 
 function changeColors(color) {
 
     for (let i = 0; i < colors.length; i++) {
         squares[i].style.backgroundColor = function () {
-            if (Array.isArray(color)) return color[i];
-            return color;
+            if (Array.isArray(color[0])) return rgbToText(color[i]);
+            return rgbToText(color);
         }();
     }
 }
@@ -116,7 +129,8 @@ function newGame(cant) {
     display(cant);
     colors = generateRandomColors(cant);
     pickedColor = pickColor();
-    colorDisplay.textContent = pickedColor;
+    colorDisplay.textContent = rgbToText(pickedColor);
+    titleDisplay.style.color = "#f3f3f3"
     messageDisplay.textContent = "";
     bgContainer.style.backgroundColor = null;
     changeColors(colors);
